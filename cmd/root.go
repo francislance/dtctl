7dbe77b9baaf67c0a53aa5562d3e3a6621d58fbf
francislance <lance.arzadon@gmail.com>
2024-12-06 10:21:21 +0700

    Add --version feature

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,29 @@
 package cmd
 
 import (
+    "fmt"
+
     "github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-    Use:   "dtctl",
-    Short: "dtctl is a CLI tool for interacting with Dependency-Track",
+    Use:     "dtctl",
+    Short:   "dtctl is a CLI tool for interacting with Dependency-Track",
+    Version: "", // Will set the version in init()
 }
 
+// Execute executes the root command.
 func Execute() error {
     return rootCmd.Execute()
 }
 
 func init() {
+    // Set the version using the GetVersion function from version.go
+    rootCmd.Version = GetVersion()
+
+    // Customize the version output format
+    rootCmd.SetVersionTemplate(fmt.Sprintf("dtctl %s\n", GetVersion()))
+
     // Add subcommands
     rootCmd.AddCommand(configCmd)
     rootCmd.AddCommand(getCmd)
---
+++ cmd/version.go
@@ -0,0 +1,10 @@
+package cmd
+
+// version holds the current version of the application.
+// Manually update this variable for each new release.
+var version = "v0.0.4"
+
+// GetVersion returns the current version.
+func GetVersion() string {
+    return version
+}
