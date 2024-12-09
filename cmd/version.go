09d4e41bba31b4784cd1b1bcefdf9950bae12fc7
francislance <lance.arzadon@gmail.com>
2024-12-09 09:54:55 +0700

    v0.0.6

--- cmd/version.go
+++ cmd/version.go
@@ -2,7 +2,7 @@ package cmd
 
 // version holds the current version of the application.
 // Manually update this variable for each new release.
-var version = "v0.0.4"
+var version = "v0.0.6"
 
 // GetVersion returns the current version.
 func GetVersion() string {
